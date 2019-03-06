8a1f1d0bc1e43171bb386d440756bc49da8cfb18
Eugene Luzgin <eluzgin@digitalguardian.com>
2019-03-06 16:16:44 -0700

    Replace Hibernate with JDBC :-)

--- src/main/java/api/subscribe/Application.java
+++ src/main/java/api/subscribe/Application.java
@@ -2,12 +2,8 @@ package api.subscribe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@EntityScan("api.subscribe.db")
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/api/subscribe/db/SubscriptionRepository.java
+++
@@ -1,8 +0,0 @@
-package api.subscribe.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
-}
